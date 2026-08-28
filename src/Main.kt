5cd97dc8d9bbe84fc806c65e8438be0ea6111534
emiliano <emiimarqz@gmail.com>
2026-08-28 00:53:21 -0700

    Asignacion Arrays and Loops
    Módulo 2: Kotlin Basics

--- src/Main.kt
+++ src/Main.kt
@@ -68,6 +68,18 @@ fun main() {
         else -> println("OK fish name")
     }
 
-    
+    // TASK 1: ARRAYS AND LOOPS
+    // Looping over arrays and lists is a fundamental technique that has a lot of flexibility in Kotlin. Let's practice.
+    // Basic example
+    // Create an integer array of numbers called numbers, from 11 to 15.
+    // Create an empty mutable list for Strings.
+    // Write a for loop that loops over the array and adds the string representation of each number to the list.
+    // Challenge example
+    // How can you use a for loop to create (a list of) the numbers between 0 and 100 that are divisible by 7?
+    val numbers = arrayOf(11, 12, 13, 14, 15)
+    val stringList = mutableListOf<String>()
+    for (number in numbers) {
+        stringList.add(number.toString())
+    }
 
 }
